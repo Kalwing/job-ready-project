c9bac228dc54ec9da88cfda577f136f0c06aceec
Kalwing <thomasdargent@gmail.com>
2018-05-02 18:11:07 +0200

    Made first chart with d3 in 7

---
+++ 7-Admin_panel/js/d3Chart.js
@@ -0,0 +1,88 @@
+function draw (data) {
+   var margin = 30,
+      width = $(".UserChartPanel").width() - margin,
+      height = $(".UserChartPanel").height() - margin;
+   var svg = d3.select(".UserChartPanel")
+               .append("svg")
+               .attr("height", height)
+               .attr("width", width)
+               .style("font-size", "0.5em")
+
+
+
+   var time_extent = d3.extent(data, row => {
+      return row['date'];
+   });
+   var value_extent = d3.extent(data, row => {
+      return row['value'];
+   });
+
+   var time_scale = d3.time.scale()
+                     .range([margin, width])
+                     .domain(time_extent)
+   var value_scale = d3.scale.linear()
+                     .range([height, margin])
+                     .domain(value_extent)
+
+   var time_axis = d3.svg.axis()
+                     .scale(time_scale)
+                     .ticks(d3.time.month,Math.floor(40*(data.length/width)));
+   //This seems to be a good formula for spacing the ticks. Mostly by trial and
+   //Error though.
+
+   var value_axis = d3.svg.axis()
+                     .scale(value_scale)
+                     .orient("left");
+   var area = d3.svg.area()
+            .x(function(d) { return time_scale(d["date"]); })
+            .y0(height)
+            .y1(function(d) { return value_scale(d["value"]); });
+   var line = d3.svg.line()
+            .x(function(d) { return time_scale(d["date"]); })
+            .y(function(d) { return value_scale(d["value"]); });
+
+   var mainGradient = svg.append('linearGradient')
+       .attr('id', 'mainGradient')
+       .attr('gradientTransform', "rotate(90)");
+
+   // Create the stops of the main gradient. Each stop will be assigned
+   // a class to style the stop using CSS.
+   mainGradient.append('stop')
+       .attr('class', 'stop-left')
+       .attr('offset', '0');
+
+   mainGradient.append('stop')
+       .attr('class', 'stop-right')
+       .attr('offset', '1');
+
+   d3.select("svg")
+      .append('g')
+      .attr('class', 'x axis')
+      .attr('transform', "translate(0,"+(height)+")")
+      .call(time_axis);
+   d3.select("svg")
+      .append('g')
+      .attr('class', 'y axis')
+      .attr('transform', "translate("+margin+", 0)")
+      .call(value_axis);
+   d3.select("svg")
+      .append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("fill", "none")
+      .attr("stroke", "url(#mainGradient)")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+   d3.select("svg")
+      .append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area);
+}
+
+format = d3.time.format("%Y-%m-%d");
+userByTime.forEach(row => {
+   row['date'] = format.parse(row['date']);
+   row['value'] = +row["value"];
+});
+draw(userByTime);
--- 7-Admin_panel/process/signalgen.py
+++ 7-Admin_panel/process/signalgen.py
@@ -1,8 +1,13 @@
 import random
 import datetime
 
+def moy(cur, last, before):
+    r = (cur + last + before) // 3
+    return r
+
 def randomDateValueJson(maxValue, minYear, maxYear, size) :
     sig = [random.randint(0, maxValue) for i in range(size)]
+    sigm = [sig[i] if i <= 1 else moy(sig[i], sig[i-1], sig[i-2]) for i in range(size)]
     dates = [datetime.date(random.randint(minYear, maxYear),
                     random.randint(1,12),
                     random.randint(1,25)) for i in range(size)]
@@ -11,11 +16,11 @@ def randomDateValueJson(maxValue, minYear, maxYear, size) :
     # Ajoute a json une ligne de type : `["date":2018-11-04,"value":39],`
     for i in range(size):
         json += "{\"date\": \"" + str(dates[i]) + "\""
-        json += ",\"value\":" + str(sig[i]) + "}"
+        json += ",\"value\":" + str(sigm[i]) + "}"
         if (i < size - 1) :
             json += ","
         json += "\n"
     return json + "]"
 
 print(randomDateValueJson(60,2015,2018,100))
-userByTime = "userByTime = " randomDateValueJson(60,2015,2018,100) + ";"
+userByTime = "userByTime = " + randomDateValueJson(60,2015,2018,100) + ";"
